12d78ad4f5d0dc43df539a4a474402ef756b79fb
Sakieye <yhkuo41@gmail.com>
2021-01-20 01:35:29 +0800

    48M. Rotate Image

--- BlindCurated75/RotateImage.java
+++ BlindCurated75/RotateImage.java
@@ -1,5 +1,7 @@
 package BlindCurated75;
 
+import java.util.Arrays;
+
 public class RotateImage {
 
   public static void main(String[] args) {
@@ -16,11 +18,32 @@ public class RotateImage {
     int[][][] outputs = {o1, o2, o3, o4};
 
     for (int i = 0; i < inputs.length; i++) {
-      System.out.println(outputs[i] == rotate(inputs[i]));
+      System.out.println(Arrays.deepToString(rotate(inputs[i])));
+      System.out.println(Arrays.deepToString(outputs[i]));
     }
   }
 
   public static int[][] rotate(int[][] matrix) {
-    return null;
+    // outermost to innermost, F(first), S(second), T(third)
+    // 1 2 3 4 5 F
+    // X 1 2 3 S X
+    // X X 1 T X X
+    // X X X X X X
+    // X X X X X X
+    // X X X X X X
+    for (int row = 0; row < matrix.length / 2; row++) {
+      for (int col = row; col <= (matrix.length - 2) - row; col++) {
+        int upperLeftMostVal = matrix[row][col]; // The upper leftmost value in this iteration
+        int preRow1 = matrix.length - 1 - col;
+        matrix[row][col] = matrix[preRow1][row];
+        int preRow2 = matrix.length - 1 - row;
+        matrix[preRow1][row] = matrix[preRow2][preRow1];
+        int preRow3 = matrix.length - 1 - preRow1;
+        matrix[preRow2][preRow1] = matrix[preRow3][preRow2];
+        matrix[preRow3][preRow2] = upperLeftMostVal;
+      }
+    }
+
+    return matrix;
   }
 }
